806753ac294872ea548ba9969eead4ea5b91853e
情 之 雁 伤 <10019800+junyan101@user.noreply.gitee.com>
2022-10-31 18:08:30 +0800

    search 查询所有表

---
+++ search.sql
@@ -0,0 +1,20 @@
+# search
+
+# 用户表
+select * from love.user;
+# 实名认证
+select * from love.user_id_card;
+# 用户伴侣
+select * from love.user_love;
+# 伴侣申请列表HistoryCompanion
+select * from love.companion_pplication_list;
+# 恋爱小记
+select * from love.small_notes;
+#恋爱小记留言
+select * from love.small_notes_message;
+# 恋爱小记—点赞
+select * from love.small_notes_kudos;
+# 重要历程 ImportantHistory
+select * from love.important_history;
+
+
